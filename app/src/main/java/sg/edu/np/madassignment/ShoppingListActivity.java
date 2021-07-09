34fb136608b733b6c302f2f0ade205e9c3e05a0e
Muhammad Aidel Sufyan Abdul Manap <aidelsufyan21@gmail.com>
2021-07-09 20:17:42 +0800

    Added onCreate, onStart, onResume, onStop, onDestroy for ShoppingListActivity.java.

--- app/src/main/java/sg/edu/np/madassignment/ShoppingListActivity.java
+++ app/src/main/java/sg/edu/np/madassignment/ShoppingListActivity.java
@@ -1,4 +1,30 @@
 package sg.edu.np.madassignment;
 
-public class ShoppingListActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShoppingListActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shoppinglist);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+    }
 }
