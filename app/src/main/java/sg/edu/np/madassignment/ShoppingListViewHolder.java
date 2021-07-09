7ee0794433345bc4084976d4e1dcb1038c5ac2d5
Muhammad Aidel Sufyan Abdul Manap <aidelsufyan21@gmail.com>
2021-07-09 19:32:53 +0800

    Added codes for ShoppingListViewHolder class

--- app/src/main/java/sg/edu/np/madassignment/ShoppingListViewHolder.java
+++ app/src/main/java/sg/edu/np/madassignment/ShoppingListViewHolder.java
@@ -1,4 +1,18 @@
 package sg.edu.np.madassignment;
 
-public class ShoppingListViewHolder {
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ShoppingListViewHolder extends RecyclerView.ViewHolder {
+    TextView foodName;
+    TextView foodCategory;
+    ImageView foodImage;
+
+    public ShoppingListViewHolder(View itemView){
+        super(itemView);
+
+    }
 }
