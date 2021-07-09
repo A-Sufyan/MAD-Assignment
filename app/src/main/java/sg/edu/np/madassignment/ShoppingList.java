01df25e4e6b0fc7e7d4d60829ca10bf9bbeb1c8a
Muhammad Aidel Sufyan Abdul Manap <aidelsufyan21@gmail.com>
2021-07-09 19:14:33 +0800

    - Created ShoppingList class - Added 3 objects for ShoppingList class: String foodName, String foodCategory, Image foodImage - Set getter and setter for class objects - Set constructor for ShoppingList class

--- app/src/main/java/sg/edu/np/madassignment/ShoppingList.java
+++ app/src/main/java/sg/edu/np/madassignment/ShoppingList.java
@@ -1,5 +1,41 @@
 package sg.edu.np.madassignment;
 
-public class ShoppingList {
+import android.media.Image;
 
+public class ShoppingList {
+    private String foodName;
+    private String foodCategory;
+    private Image foodImage;
+    //private String foodDescription
+    //private Integer foodQuantity
+
+    public String getFoodName() {
+        return foodName;
+    }
+
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
+
+    public String getFoodCategory() {
+        return foodCategory;
+    }
+
+    public void setFoodCategory(String foodCategory) {
+        this.foodCategory = foodCategory;
+    }
+
+    public Image getFoodImage() {
+        return foodImage;
+    }
+
+    public void setFoodImage(Image foodImage) {
+        this.foodImage = foodImage;
+    }
+
+    public ShoppingList(String foodName, String foodCategory, Image foodImage) {
+        this.foodName = foodName;
+        this.foodCategory = foodCategory;
+        this.foodImage = foodImage;
+    }
 }
