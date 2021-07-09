42cdc8d4cfa4bb964571cb646696ba149d1f9451
Jing Yu Tan <s10204382@connect.np.edu.sg>
2021-07-09 14:47:10 +0800

    Modified CalculatorActivity.java, changed BG color in activity_calculator.xml, added graypink color to strings.xml

--- app/src/main/java/sg/edu/np/madassignment/CalculatorActivity.java
+++ app/src/main/java/sg/edu/np/madassignment/CalculatorActivity.java
@@ -10,6 +10,9 @@ public class CalculatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
     @Override
     protected void onStart(){
