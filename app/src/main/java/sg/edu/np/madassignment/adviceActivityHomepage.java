03078f04304c7def14c9a9809e2aefdbca9aeaa2
Jing Yu Tan <s10204382@connect.np.edu.sg>
2021-07-12 21:38:28 +0800

    Added activity_advice_layout.xml

--- app/src/main/java/sg/edu/np/madassignment/adviceActivityHomepage.java
+++ app/src/main/java/sg/edu/np/madassignment/adviceActivityHomepage.java
@@ -11,4 +11,21 @@ public class adviceActivityHomepage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advice_homepage);
     }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+    }
 }
