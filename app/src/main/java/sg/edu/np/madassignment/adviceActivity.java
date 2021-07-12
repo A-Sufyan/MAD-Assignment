9822ff5ecdbb997f05d1572e11cf35a14a65846c
Jing Yu Tan <s10204382@connect.np.edu.sg>
2021-07-12 23:01:49 +0800

    Implemented onClickListeners in adviceActivityAdapter

--- app/src/main/java/sg/edu/np/madassignment/adviceActivity.java
+++ app/src/main/java/sg/edu/np/madassignment/adviceActivity.java
@@ -10,6 +10,7 @@ public class adviceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advice);
+        ShoppingList newProduct = new ShoppingList();
 
     }
 
