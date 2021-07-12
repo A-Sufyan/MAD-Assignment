bf8d72cc82c8c1cd4874b3bcadba42e8c39242a5
Jing Yu Tan <s10204382@connect.np.edu.sg>
2021-07-12 23:12:54 +0800

    Implemented dynamic changing icon in adviceActivity.java

--- app/src/main/java/sg/edu/np/madassignment/adviceActivity.java
+++ app/src/main/java/sg/edu/np/madassignment/adviceActivity.java
@@ -3,14 +3,42 @@ package sg.edu.np.madassignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class adviceActivity extends AppCompatActivity {
+    private final static String TAG = "Advice Activty";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.v(TAG, "On Create Advice Activity");
+
         setContentView(R.layout.activity_advice);
         ShoppingList newProduct = new ShoppingList();
+        Bundle recieveData = getIntent().getExtras();
+
+        TextView productName = findViewById(R.id.adviceActivityitemTitle);
+        TextView productDesc = findViewById(R.id.adviceActivityDescription);
+        ImageView productIcon = findViewById(R.id.adviceActivityProductIcon);
+
+        String recievedName = recieveData.getString("productname");
+        String recievedDesc = recieveData.getString("productdesc");
+        String recievedCategory = recieveData.getString("productcategory");
+
+        if(recieveData.getString("productcategory") == "f"){
+            productIcon.setImageResource(R.drawable.);
+        }
+        else if (recieveData.getString("productcategory") == "d"){
+            productIcon.setImageResource();
+
+        }
+        else if (recieveData.getString("productcategory") == "h"){
+            productIcon.setImageResource();
+
+        }
+
 
     }
 
