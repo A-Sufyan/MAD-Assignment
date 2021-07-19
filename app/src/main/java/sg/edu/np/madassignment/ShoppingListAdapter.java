43ccbebf0b25e339f4da3891cc929b0b93671768
Muhammad Aidel Sufyan Abdul Manap <aidelsufyan21@gmail.com>
2021-07-19 13:27:30 +0800

    Swapped item amount and price

--- app/src/main/java/sg/edu/np/madassignment/ShoppingListAdapter.java
+++ app/src/main/java/sg/edu/np/madassignment/ShoppingListAdapter.java
@@ -42,32 +42,31 @@ public class ShoppingListAdapter extends RecyclerView.Adapter<ShoppingListViewHo
         ShoppingList shoppingItem = shoppingData.get(position);
         if (shoppingItem.getItemCategory().equals("F")) {
             holder.itemName.setText(shoppingItem.getItemName());
-            holder.itemAmount.setText('$' + String.valueOf(String.format("%.2f", shoppingItem.getItemAmount())));
-            holder.itemPrice.setText(String.valueOf(shoppingItem.getItemPrice()) + " (g)");
+            holder.itemAmount.setText('$' + String.valueOf(String.format("%.2f", shoppingItem.getItemPrice())));
+            holder.itemPrice.setText(String.valueOf(shoppingItem.getItemAmount()) + " (g)");
             holder.itemImage.setImageResource(R.drawable.ic_food);
         }
         else if (shoppingItem.getItemCategory().equals("D")) {
             holder.itemName.setText(shoppingItem.getItemName());
-            holder.itemAmount.setText('$' + String.valueOf(String.format("%.2f", shoppingItem.getItemAmount())));
-            holder.itemPrice.setText(String.valueOf(shoppingItem.getItemPrice()) + " (g)");
+            holder.itemAmount.setText('$' + String.valueOf(String.format("%.2f", shoppingItem.getItemPrice())));
+            holder.itemPrice.setText(String.valueOf(shoppingItem.getItemAmount()) + " (g)");
             holder.itemImage.setImageResource(R.drawable.ic_drinks);
         }
         else if (shoppingItem.getItemCategory().equals("H")) {
             holder.itemName.setText(shoppingItem.getItemName());
-            holder.itemAmount.setText('$' + String.valueOf(String.format("%.2f", shoppingItem.getItemAmount())));
-            holder.itemPrice.setText(String.valueOf(shoppingItem.getItemPrice()) + " (g)");
+            holder.itemAmount.setText('$' + String.valueOf(String.format("%.2f", shoppingItem.getItemPrice())));
+            holder.itemPrice.setText(String.valueOf(shoppingItem.getItemAmount()) + " (g)");
             holder.itemImage.setImageResource(R.drawable.ic_household_items);
         }
         else if (shoppingItem.getItemCategory().equals("P")) {
             holder.itemName.setText(shoppingItem.getItemName());
-            holder.itemAmount.setText('$' + String.valueOf(String.format("%.2f", shoppingItem.getItemAmount())));
-            holder.itemPrice.setText(String.valueOf(shoppingItem.getItemPrice()) + " (g)");
+            holder.itemAmount.setText('$' + String.valueOf(String.format("%.2f", shoppingItem.getItemPrice())));
+            holder.itemPrice.setText(String.valueOf(shoppingItem.getItemAmount()) + " (g)");
             holder.itemImage.setImageResource(R.drawable.ic_pet_items);
         }
         else {
             holder.itemName.setText("Item not found!");
         }
-        //holder.foodImage.setImageURI(s); <-- TODO need to find solution to bind image for foodImage
     }
 
     public int getItemCount(){
