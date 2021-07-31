8bc92abd24814e14f3ffdbc7e00c5bba7cf5b58c
Jing Yu Tan <s10204382@connect.np.edu.sg>
2021-07-31 11:54:26 +0800

    CompareActivity.java: Add toast to show "Added to Compare!" when clicking Add to Compare Button

--- app/src/main/java/sg/edu/np/worthornot/CompareActivity.java
+++ app/src/main/java/sg/edu/np/worthornot/CompareActivity.java
@@ -14,6 +14,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
@@ -68,6 +69,8 @@ public class CompareActivity extends AppCompatActivity {
                 temporaryItem.setBrand(brandInput.getText().toString());
                 compareList.add(temporaryItem);
                 compareAdapter.notifyDataSetChanged();
+                Toast.makeText(CompareActivity.this,
+                        "Added to Compare!", Toast.LENGTH_SHORT).show();
             }
         });
 
